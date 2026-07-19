933a9a663c65a7f888d2b6f1c557a58b731c0bdc
rakib1248 <rakib25059159@gmail.com>
2026-07-19 12:57:16 +0300

    fix: enable CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://enter-price-library-frontend.vercel.app/', // আসল Vercel domain বসান
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
